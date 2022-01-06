ea1f25f097b7b85dd28fad41f9fff2848a77b051
Florian Isopp <florian.isopp@student.tugraz.at>
2022-01-06 23:44:46 +0100

    init

---
+++ hcalc.cpp
@@ -0,0 +1,18 @@
+/*
+* Multiple line
+* comment
+*/
+#include<iostream>
+
+//Single line comment
+using namespace std;
+
+//This is where the execution of program begins
+int main()
+{
+   // displays Hello World! on screen
+   cout<<"Hello World!\n";
+
+   return 0;
+}
+
